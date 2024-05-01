9f6fab8eabdf06460c7aefbf329b6d5472f46852
Mateus Vilaverde <matvilaverde@casuldev.com>
2024-05-01 13:11:48 -0300

    add ngForm and navigate on form success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    CadastroComponent,
+    FooterComponent,
+    HeaderComponent,
+    SucessoCadastroComponent
+  ],
+  imports: [AppRoutingModule, BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
